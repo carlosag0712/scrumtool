b2248f71ff29afc40be31ead2fb2812557fffc73
carlos arosemena <ceag68@hotmail.com>
2018-02-03 07:52:43 -0500

    first commit

---
+++ src/main/java/io/agileintelligence/scrumtool/models/Company.java
@@ -0,0 +1,8 @@
+package io.agileintelligence.scrumtool.models;
+
+public class Company {
+}
+
+
+//A Company has many departments
+//A Department has 1 portfolio
---
+++ src/test/java/io/agileintelligence/scrumtool/ScrumtoolApplicationTests.java
@@ -0,0 +1,16 @@
+package io.agileintelligence.scrumtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScrumtoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
